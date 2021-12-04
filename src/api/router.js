c4f8eeaef70db362e290fdcdf67ba57866a6de3b
mykhailo-vaskivnyuk <m.vaskivnyuk@gmail.com>
2021-12-04 11:18:37 +0200

    fix: del comments

--- src/api/router.js
+++ src/api/router.js
@@ -1,17 +1,8 @@
 const Router = require('@koa/router');
 const { healthRoute } = require('./routes/healhRoute');
-// const { getService } = require('../middlewares/getService');
-// const { serviceRoute } = require('./routes/serviceRoute');
 
 const router = new Router();
 
-router
-  .get('/health', healthRoute);
-// .use(async (ctx) => {
-//   console.log(ctx.path);
-//   ctx.body = 'RESPONSE';
-// })
-// .use(getService)
-// .use(serviceRoute);
+router.get('/health', healthRoute);
 
 module.exports = { router };
